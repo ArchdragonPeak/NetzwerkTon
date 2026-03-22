044f604101c1796ab504fb8c7f0584c73566a596
ArchdragonPeak <erzdrachengipfel@gmail.com>
2026-03-22 22:22:02 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+
+
+class Program
+{
+  static void RunServer()
+  {
+
+  }
+  
+  static void Main(string[] args)
+  {
+    if (args.Length > 0)
+    {
+      switch (args[0])
+      {
+        case "server":
+          RunServer();
+          break;
+        // add client
+        default:
+          Console.WriteLine($"Unknown argument: {args[0]}");
+          break;
+      }
+    }
+    else
+    {
+      Console.WriteLine("No arguments provided.");
+    }
+  }
+}
